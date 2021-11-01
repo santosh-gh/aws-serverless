165f37f8ee7944bd92bd1bf0ff19dd7b1b7445df
santosh kumar mohapatra <santosh.mohapatra25@.com>
2021-11-01 09:21:29 +0000

    s

--- live/skillsit-s3-website/main.tf
+++ live/skillsit-s3-website/main.tf
@@ -1,9 +1,13 @@
-provider "aws" {}
+provider "aws" {
+  region = "ap-south-1"
+  shared_credentials_file = "C:\\AWS\\bin\\credentials"
+  profile = "default"
+}
 
 module "s3-website" {
   source = "../../modules/s3-website"
 
-  bucket_name = "skillsit-s3-website"
+  bucket_name = "mys3-web0"
 }
 
 output "website_endpoint" {
