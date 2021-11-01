82e5962f42a537cd472b7e087b40447093aa8473
santosh kumar mohapatra <santosh.mohapatra25@.com>
2021-11-01 16:17:08 +0000

    te

--- live/skillsit-s3-website/main.tf
+++ live/skillsit-s3-website/main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "ap-south-1"
   shared_credentials_file = "C:\\AWS\\bin\\credentials"
   profile = "default"
-  
+
 }
 
 module "s3-website" {
   source = "../../modules/s3-website"
 
-  bucket_name = "mys3-web0"
+  //bucket_name = "mys3-web0"
 }
 
 output "website_endpoint" {
--- modules/s3-website/variables.tf
+++ modules/s3-website/variables.tf
@@ -1,3 +1,4 @@
 variable "bucket_name" {
+  default = "mys3-web10"
   description = "The name of the AWS S3 bucket this website will be published to."
 }
